fa1759c074d1ea3f3e55165043a2a6d608505c0c
veereshp1 <veereshpatil1997@gmail.com>
2024-08-29 16:00:13 +0530

    add_part_A

---
+++ newDemo/demo/src/main/java/git_demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package git_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
